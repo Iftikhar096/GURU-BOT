1f14527ec4fca8b9fc669bebc75c46cb5d713408
Iftikhar096 <145902885+Iftikhar096@users.noreply.github.com>
2023-09-27 13:54:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923345463830', 'Iftikhar', true],
+  ['923167075096', 'Inoxcent', true], 
   [''] 
 ] //Number of owners
 
